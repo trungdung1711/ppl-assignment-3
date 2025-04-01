ce6e6bb65ff661961b7daa021a4d4261aaa2a772
zun <trungdunglebui17112004@gmail.com>
2025-04-01 08:47:37 +0700

    dung-doing research about Scope/Object/Type data structure in Go

---
+++ tests/go/my-simple-app/go.mod
@@ -0,0 +1,7 @@
+module my-simple-app
+
+go 1.23.6
+
+require github.com/trungdung1711/go-inspection v0.0.0
+
+replace github.com/trungdung1711/go-inspection => /home/zun/code/go/go-inspection
---
+++ tests/go/my-simple-app/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"os"
+
+	"github.com/trungdung1711/go-inspection/inspect"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename.go>")
+		return
+	}
+	filename := os.Args[1]
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	if err != nil {
+		fmt.Printf("Error parsing file: %v\n", err)
+		os.Exit(1)
+	}
+	info := &types.Info{
+		Defs:   make(map[*ast.Ident]types.Object),
+		Uses:   make(map[*ast.Ident]types.Object),
+		Scopes: make(map[ast.Node]*types.Scope),
+	}
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check("main", fset, []*ast.File{file}, info)
+	if err != nil {
+		fmt.Printf("Type checking error: %v\n", err)
+		os.Exit(1)
+	}
+
+	inspect.PrintScope(pkg.Scope(), 0)
+}
